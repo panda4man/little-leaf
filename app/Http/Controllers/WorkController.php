61a0963a7e17c281113130d7076e01ca6455e1f6
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-06 15:36:26 -0500

    use new search class

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -3,21 +3,33 @@
 namespace App\Http\Controllers;
 
 
-use App\Services\WorkService;
+use App\Http\Request\Queries\WorkSearchRequest;
+use App\Searches\WorkSearch;
 use App\Transformers\WorkTransformer;
 use Carbon\Carbon;
 
 class WorkController extends Controller
 {
     /**
-     * @param WorkService $service
+     * Route all requests to the default month view for today's month
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function index()
+    {
+        $today = Carbon::now()->toDateString();
+
+        return redirect()->route('work-monthly', ['date' => $today]);
+    }
+
+    /**
+     * @param WorkSearchRequest $req
+     * @param WorkSearch $search
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function getMonthView(WorkService $service)
+    public function getMonthView(WorkSearchRequest $req, WorkSearch $search)
     {
-        $date = request()->get('date');
-        $date = Carbon::createFromFormat('Y-m-d', $date);
-        $works = $service->forMonth($date->year, $date->month);
+        $works = $search->params($req)->month()->search();
         $works = fractal()->collection($works, new WorkTransformer())->toArray();
 
         return view('work.month', [
@@ -25,13 +37,33 @@ class WorkController extends Controller
         ]);
     }
 
-    public function getWeekView(WorkService $service)
+    /**
+     * @param WorkSearchRequest $req
+     * @param WorkSearch $search
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getWeekView(WorkSearchRequest $req, WorkSearch $search)
     {
-        return view('work.week');
+        $works = $search->params($req)->week()->search();
+        $works = fractal()->collection($works, new WorkTransformer())->toArray();
+
+        return view('work.week', [
+            'work' => $works
+        ]);
     }
 
-    public function getDayView(WorkService $service)
+    /**
+     * @param WorkSearchRequest $req
+     * @param WorkSearch $search
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function getDayView(WorkSearchRequest $req, WorkSearch $search)
     {
-        return view('work.day');
+        $works = $search->params($req)->day()->search();
+        $works = fractal()->collection($works, new WorkTransformer())->toArray();
+
+        return view('work.day', [
+            'work' => $works
+        ]);
     }
 }
