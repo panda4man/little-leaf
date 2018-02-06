42bd48df2c0dd2938b8be4efad64144804af0a66
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-06 17:03:09 -0500

    blade updates

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -8,7 +8,8 @@
             <v-flex xs12 sm4 m4 lg3>
                 <v-card>
                     <v-list>
-                        <v-list-tile avatar @click="selectCompany(c.id)" v-for="company in companies" :key="company.id">
+                        <v-subheader style="justify-content: space-between">Companies <v-btn flat icon color="green" @click="openCreateModal"><v-icon>add_circle</v-icon></v-btn></v-subheader>
+                        <v-list-tile avatar @click="selectCompany(company.id)" v-for="company in companies" :key="company.id">
                             <v-list-tile-avatar>
                                 <img src="http://placehold.it/200x200">
                             </v-list-tile-avatar>
@@ -48,12 +49,41 @@
                                 <h3 class="headline">Clients</h3>
                             </v-card-title>
                             <v-card-text>
-
+                                <v-list>
+                                    <v-list-tile v-for="client in currentCompany.clients" :key="client.id">
+                                        <v-list-tile-content>
+                                            <v-list-tile-title v-text="client.name"></v-list-tile-title>
+                                        </v-list-tile-content>
+                                    </v-list-tile>
+                                </v-list>
                             </v-card-text>
                         </v-card>
                     </v-flex>
                 </v-layout>
             </v-flex>
+            <v-dialog v-model="modals.createModal" max-width="600">
+                <v-card>
+                    <v-card-title>
+                        Create New Company
+                    </v-card-title>
+                    <v-card-text>
+                        <form method="POST" data-vv-scope="create">
+                            <form-errors v-if="formErrors && formErrors.create" :errors="formErrors.create"></form-errors>
+                            <v-text-field
+                                label="Company Name"
+                                name="name"
+                                v-model="forms.newCompany.name"
+                                :error-messages="errors.collect('name', 'create')"
+                                v-validate="'required'">
+                            </v-text-field>
+                        </form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn @click.stop="validateCreateCompany" color="primary">Create</v-btn>
+                        <v-btn @click.stop="closeCreateModal" flat>Cancel</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-layout>
     </companies>
 @endsection
