d538afe24c7c9ba8b8ba455418355d0c9ec2d910
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-06 15:36:16 -0500

    filled out controllers

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -2,10 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Transformers\CompanyTransformer;
+
 class CompaniesController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
-        return view('companies.index');
+        $companies = Company::orderBy('name')->get();
+        $companies = fractal()
+            ->collection($companies, new CompanyTransformer())
+            ->includeClients()
+            ->toArray();
+
+        return view('companies.index', [
+            'companies' => $companies
+        ]);
+    }
+
+    /**
+     * @param Company $company
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function getSelect(Company $company)
+    {
+        session('company', $company->id);
+
+        session()->flash('success', 'Switched to ' . $company->name);
+        return redirect()->back();
     }
 }
