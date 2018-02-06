6b76eab04500c8772c7b8f4f66144e99db3659ac
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-06 15:40:49 -0500

    new routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,18 @@ Auth::routes();
 Route::view('/', 'welcome');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware' => 'auth'], function () {
-    // Render base companies view
+    // Company routes
     Route::get('/companies', 'CompaniesController@index');
+    Route::get('/companies/{company}/select', ['as' => 'set-company', 'uses' => 'CompaniesController@getSelect']);
 
     // Render base clients view
     Route::get('/clients', 'ClientsController@index');
 
     // Work View Routes
-    Route::get('/work/month', 'WorkController@getMonthView');
-    Route::get('/work/week', 'WorkController@getWeekView');
-    Route::get('/work/day', 'WorkController@getDayView');
+    Route::get('/work', 'WorkController@index');
+    Route::get('/work/month', ['as' => 'work-monthly', 'uses' => 'WorkController@getMonthView']);
+    Route::get('/work/week', ['as' => 'work-weekly', 'uses' => 'WorkController@getWeekView']);
+    Route::get('/work/day', ['as' => 'work-dayly', 'uses' => 'WorkController@getDayView']);
 
     Route::group(['prefix' => 'ajax', 'namespace' => 'Ajax'], function () {
         Route::post('/companies', 'CompaniesController@store');
