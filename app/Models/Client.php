22d902d237195a09015b009ccc838393db66261c
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-08 09:05:59 -0500

    use soft deletes

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,13 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * @var array
+     */
     protected $fillable = [
         'name', 'address', 'city', 'state', 'zip', 'country'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function company()
     {
         return $this->belongsTo(Company::class);
