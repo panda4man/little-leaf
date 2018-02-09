c9f1a8660a49a2c3dfa68fc45d9062d8936226a8
Andrew Clinton <ddrew.clinton@gmail.com>
2018-02-08 22:00:44 -0500

    send data in json format

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Client;
+use App\Models\Company;
+use App\Transformers\ClientTransformer;
+use App\Transformers\CompanyTransformer;
 
 class ClientsController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $clients = Client::query();
+        $companies = Company::orderBy('name')->get();
 
         if(request()->has('company_id')) {
             $clients->whereHas('company', function ($q) {
@@ -16,10 +23,27 @@ class ClientsController extends Controller
             });
         }
 
-        $clients = $clients->get();
+        $clients = $clients->orderBy('name')->get();
+        $clients = fractal()->collection($clients, new ClientTransformer())->includeCompany()->toArray();
+        $companies = fractal()->collection($companies, new CompanyTransformer())->toArray();
 
         return view('clients.index', [
-            'clients' => $clients
+            'clients'   => $clients,
+            'companies' => $companies,
+        ]);
+    }
+
+    /**
+     * @param Client $client
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Client $client)
+    {
+        $clientJson = fractal()->item($client, new ClientTransformer())->includeCompany()->toArray();
+
+        return view('clients.show', [
+            'client' => $client,
+            'json'   => $clientJson,
         ]);
     }
 }
